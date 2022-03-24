594d6699e88449869a4df447e53501fcdfa84a6b
Anna Williamson <awilliamson@caseguard.com>
2022-03-24 16:57:09 -0400

    add theme

---
+++ RockStarProject/MVVM/MaterialViewModel.cs
@@ -0,0 +1,50 @@
+﻿using RockStarProject.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RockStarProject.MVVM
+{
+    public class MaterialViewModel : Notifiable
+    {
+        private ICommand selectedMaterialChangedCommand;
+        public ICommand SelectedMaterialChangedCommand
+        {
+            get
+            {
+                if (selectedMaterialChangedCommand == null)
+                    selectedMaterialChangedCommand = new RelayCommand(p => MaterialChanged(p));
+                return selectedMaterialChangedCommand;
+            }
+        }
+
+        private string materialName = "defualt";
+        public string MaterialName
+        {
+            get
+            {
+                return materialName;
+            }
+            set
+            {
+                materialName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void MaterialChanged(object param)
+        {
+            MaterialName = (string)param;
+        }
+
+        public MaterialViewModel()
+        {
+
+        }
+
+        
+    }
+}
--- RockStarProject/MainWindow.xaml.cs
+++ RockStarProject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RockStarProject.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace RockStarProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MaterialViewModel ViewModel { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = (MaterialViewModel)DataContext;
         }
     }
 }
