ba457df63af7b6fda3dab1afe964203d584d69c0
Anna <annajoywtech@gmail.com>
2022-03-26 13:31:59 -0400

    merge

--- RockStarProject/MVVM/MaterialViewModel.cs
+++ RockStarProject/MVVM/MaterialViewModel.cs
@@ -23,9 +23,21 @@ namespace RockStarProject.MVVM
             }
         }
 
+        private ICommand getNextTextureCommand;
+        public ICommand GetNextTextureCommand
+        {
+            get
+            {
+                if (getNextTextureCommand == null)
+                    getNextTextureCommand = new RelayCommand(p => GetNextTexture(p));
+                return getNextTextureCommand;
+            }
+        }
+
         #endregion
 
         #region Model Properties
+
         public MaterialModel MaterialModel { get; set; } = new MaterialModel();
 
         private bool isFavoriteChecked;
@@ -45,7 +57,7 @@ namespace RockStarProject.MVVM
             }
         }
 
-        private string materialName = "defualt";
+        private string materialName = "Blue Agate";
         public string MaterialName
         {
             get
@@ -54,16 +66,20 @@ namespace RockStarProject.MVVM
             }
             set
             {
+                if (materialName == value)
+                    return;
                 materialName = value;
                 OnPropertyChanged();
+                GetNextTexture(null);
             }
         }
 
         #endregion
 
         #region View Properties
-        private string imageSource = "defualt";
-        public string ImageSource
+
+        private string imageSourceMaterialBall = "../../Images/Blue_agate/material_ball.png";
+        public string ImageSourceMaterialBall
         {
             get
             {
@@ -71,13 +87,49 @@ namespace RockStarProject.MVVM
             }
             set
             {
+                if (imageSourceMaterialBall == value)
+                    return;
                 imageSourceMaterialBall = value;
                 OnPropertyChanged();
             }
         }
+        private string imageSourceTexture = "../../Images/Blue_agate/normal.png";
+        public string ImageSourceTexture
+        {
+            get
+            {
+                return imageSourceTexture;
+            }
+            set
+            {
+                if (imageSourceTexture == value)
+                    return;
+                imageSourceTexture = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string textureLable = "Normal";
+        public string TextureLable
+        {
+            get
+            {
+                return textureLable;
+            }
+            set
+            {
+                if (textureLable == value)
+                    return;
+                textureLable = value;
+                OnPropertyChanged();
+
+            }
+        }
+
         #endregion
 
         #region Methods
+
         private void MaterialChanged(object param)
         {
             MaterialName = (string)param;
@@ -85,7 +137,7 @@ namespace RockStarProject.MVVM
             {
                 case "Blue Agate":
                     ImageSourceMaterialBall = "../../Images/Blue_agate/material_ball.png";
-                        break;
+                    break;
                 case "Copper":
                     ImageSourceMaterialBall = "../../Images/Copper/material_ball.png";
                     break;
@@ -100,13 +152,53 @@ namespace RockStarProject.MVVM
                     break;
             }
         }
+
+        private void GetNextTexture(object param)
+        {
+            var material = MaterialName;
+            if (material == "Blue Agate")
+                material = "Blue_agate";
+
+            var currentTexture = ImageSourceTexture.Substring(ImageSourceTexture.Length - 10);
+
+            if (!ImageSourceTexture.Contains(material))
+            {
+                ImageSourceTexture = "../../Images/" + material + "/normal.png";
+                TextureLable = "Normal";
+                return;
+            }
+                
+            if (currentTexture == "normal.png")
+            {
+                ImageSourceTexture = "../../Images/" + material + "/material.png";
+                TextureLable = "Material";
+                return;
+            }
+
+            else if (currentTexture == "terial.png")
+            {
+                ImageSourceTexture = "../../Images/" + material + "/albedo.png";
+                TextureLable = "Albedo";
+                return;
+            }
+
+            else if (currentTexture == "albedo.png")
+            {
+                ImageSourceTexture = "../../Images/" + material + "/normal.png";
+                TextureLable = "Normal";
+                return;
+            }
+        }
+
         #endregion
 
         #region Constructor
+
         public MaterialViewModel()
         {
 
         }
+
         #endregion
     }
 }
